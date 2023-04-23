0c4c3094c40985f06ac15eb740e4de357d80ae08
Tsvetan Tsvetanov <tsvetan@designtechnologies.com>
2023-04-23 11:03:06 +0300

    Practice 2: Parse HTTP POST

--- echo_client.py
+++ echo_client.py
@@ -1,10 +1,21 @@
 import socket
 import sys
 
+def construct_http_request(msg):
+  return (
+    "POST / HTTP/1.1\r\n"
+    "Host: localhost:1337\r\n"
+    "User-Agent: curl/7.87.0\r\n"
+    "Accept: */*\r\n"
+    f"Content-Length: {len(msg)}\r\n"
+    "Content-Type: application/x-www-form-urlencoded\r\n\r\n"
+    f"{msg}\r\n"
+  )
+
 def send_msg(msg):
   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect(('localhost', 1337))
-    s.sendall(str.encode(msg))
+    s.sendall(str.encode(construct_http_request(msg)))
     data = s.recv(1024)
 
   print('Received', repr(data))
--- echo_server.py
+++ echo_server.py
@@ -1,12 +1,21 @@
 import socket
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-  s.bind(('localhost', 1337))
-  s.listen()
-  while True:
-    conn, addr = s.accept()
-    with conn:
-      while True:
-        data = conn.recv(1024)
-        if not data: break
-        conn.sendall(data)
+from http_parser.http import HttpStream, NoMoreData
+from http_parser.reader import SocketReader
+
+def handle_connection(conn):
+  with conn:
+    r = SocketReader(conn)
+    p = HttpStream(r)
+    conn.sendall(p.body_string())
+
+def start_listening():
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(('localhost', 1337))
+    s.listen()
+    while True:
+      conn, addr = s.accept()
+      handle_connection(conn)
+
+if __name__ == "__main__":
+  start_listening()
