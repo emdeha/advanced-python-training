9e647456ddd035bcc9c2acaa632d09a74d3044eb
Tsvetan Tsvetanov <tsvetan@designtechnologies.com>
2023-04-24 11:09:58 +0300

    Practice 8: Add Celery

--- src/ticker/flask_view.py
+++ src/ticker/flask_view.py
@@ -2,11 +2,16 @@ from flask.views import MethodView
 
 from .usecase import run
 from myflask.auth import auth_api_key
+from tasks.show_message import show_message
 
 class TickerAPI(MethodView):
   decorators=[auth_api_key]
 
   def post(self, symbol: str) -> str:
     (open, financialCurrency) = run(symbol)
+
+    if 'VMW' == symbol:
+      # mypy complains that delay isn't defined for some reason, so we ignore it
+      show_message.delay(symbol) # type: ignore[attr-defined]
 
     return f'Price for {symbol} - {open} {financialCurrency}'
--- tests.bash
+++ tests.bash
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-cd src && mypy --explicit-package-bases . && cd ..
+# Check types
+cd src && mypy --explicit-package-bases .; cd ..
 
+# Run server
 python src/main.py > /dev/null 2>&1 &
 pid=$!
 
-sleep 0.1
+# Run celery
+cd src && celery -A make_celery worker --loglevel INFO &
 
+pwd
+
+sleep 1
+
+# Run tests
 python src/bin/echo_client.py asdfff test /
 python src/bin/echo_client.py asd test123 /
 python src/bin/echo_client.py asd test123 /ticker/MSFT
+python src/bin/echo_client.py asd test123 /ticker/VMW
 python src/bin/echo_client.py asd test123 /tick
 
+sleep 1
+
+# Kill long-running services
 kill $pid
+
+cd src && celery -A make_celery control shutdown
+cd ..
