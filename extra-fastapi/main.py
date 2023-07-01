ad9e24d1b2b3435a8e89de6182f9cf2676ca2d3e
Tsvetan Tsvetanov <tsvetan@camplight.net>
2023-07-01 14:53:38 +0300

    Implements ticker with FastAPI

--- extra-fastapi/main.py
+++ extra-fastapi/main.py
@@ -1,7 +1,19 @@
+from aiohttp import ClientSession
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-  return {"Hello": "World"}
+@app.get("/ticker/{symbol}")
+async def ticker(symbol):
+  async with ClientSession() as session:
+    async with session.get(
+        f'https://query2.finance.yahoo.com/v10/finance/quoteSummary/{symbol}?modules=financialData',
+      ) as response:
+
+      content = await response.json()
+
+      quote = content['quoteSummary']['result'][0]['financialData']
+      open = quote['currentPrice']['fmt']
+      financialCurrency = quote['financialCurrency']
+
+      return f'Price for {symbol} - {open} {financialCurrency}'
